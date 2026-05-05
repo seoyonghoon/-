48e9b42bd4f1c93c42119c0fa08bd83451bdbe96
seoyonghoon <34_24@kbs.hs.kr>
2026-05-05 07:17:50 +0000

    수정 내용

--- practice.py
+++ practice.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-N = 100 #경사하강법 반복횟수
-alpha = 0.1 #학습률
+N = 10000 #경사하강법 반복횟수
+alpha = 0.0001 #학습률
 alt = np.array([1, 2, 3]) #해발고도 리스트
-grad = np.array([4, 5, 6]) #경사 리스트(각 x, dy/dx 기울기o)
-v = np.array([7, 8, 9]) #속도 리스트, 얘네들 싹다 손으로 작성...?
+grad = np.array([3, 7, 5]) #경사 리스트(각 x, dy/dx 기울기o)
+v = np.array([3, 38, 15]) #속도 리스트, 얘네들 싹다 손으로 작성...?
 polynomialC = np.random.normal(0, 1, 6)
 polynomialV = [alt**2, alt*grad, grad**2, alt, grad, np.ones(len(alt))]
 
@@ -16,6 +16,6 @@ for _ in range(N):
     DelCL = np.array([np.dot(deviation, polynomialV[i]) for i in range(6)])
     polynomialC -= alpha * DelCL
 
-print(polynomialC)
+print(polynomial(1,3))
 
 
